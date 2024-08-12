7e4d66508a8bf33ad212d33a8083ce48dd0b8ae8
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2024-08-12 21:01:32 +0530

    Server CI/CD Update

--- src/main/java/com/max/quizspring/config/SwaggerConfig.java
+++ src/main/java/com/max/quizspring/config/SwaggerConfig.java
@@ -33,7 +33,7 @@ public class SwaggerConfig {
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
-                .servers(List.of(new Server().url("http://localhost:7777")))
+                .servers(List.of(new Server().url("http://43.205.149.80:7777")))
                 .addSecurityItem(new SecurityRequirement()
                         .addList("bearerAuth"))
                 .components(new Components()
